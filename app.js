9e9fb0ce2bd84b7efaf5a7e27decde9c68691220
iita71737 <iita71737@gmail.com>
2021-09-14 17:28:38 +0800

    feat: add bcrypt.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-
+const flash = require('connect-flash')
 const routes = require('./routes')
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
 require('./config/mongoose')
 
 const app = express()
@@ -11,9 +14,28 @@ const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+  // 你可以在這裡 console.log(req.user) 等資訊來觀察
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  next()
+})
 app.use(routes)
 
 app.listen(PORT, () => {
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,11 @@ const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/users')
 
-router.use('/', home)
-router.use('/todos', todos)
+const { authenticator } = require('../middleware/auth')
+
+
+router.use('/todos', authenticator, todos) // 加入驗證程序
 router.use('/users', users)
+router.use('/', authenticator, home) // 加入驗證程序
 
 module.exports = router
